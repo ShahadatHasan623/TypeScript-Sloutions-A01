805c7a7e1ec9d0aff78f5aa7dbfa5bb25cb634e2
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-05-06 23:31:04 +0600

    added filtering even number

---
+++ src/solutions.ts
@@ -0,0 +1,14 @@
+//Problem-01
+//Even Number Array
+const isEven:number[] =[]
+const filterEvenNumbers=(even:number[])=>{
+    even.map((n:number)=>{
+        if(n%2 === 0){
+            isEven.push(n)
+        }
+    })
+    return isEven
+}
+
+const even =filterEvenNumbers([1, 2, 3, 4, 5, 6])
+console.log(even);
